e57591c27525fda855213573d5cf9b35222fee24
Miaomiao Zhang <104283888+MiaomiaoZhang97@users.noreply.github.com>
2024-06-25 16:10:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
+from collections import deque
+import utils
+import numpy as np
+import gym
 import numpy as np
 import torch
 import gym
 import argparse
 import os
-
 import utils
-import TD3
-import DDPG
-import DDPG_N
+import random
 import TD3_N
 
 
-# Runs policy for X episodes and returns average reward
-# A fixed seed is used for the eval environment
-def eval_policy(policy, env_name, seed, eval_episodes=10):
-	eval_env = gym.make(env_name)
-	eval_env.seed(seed + 100)
+def eval_policy(policy, env_name, seed, eval_episodes=10, eval_cnt=None):
+    eval_env = gym.make(env_name)
+    eval_env.seed(seed + 100)
+    avg_reward = 0.
+    for episode_idx in range(eval_episodes):
+        state, done = eval_env.reset(), False
+        while not done:
+            action = policy.select_action(np.array(state))
+            next_state, reward, done, _ = eval_env.step(action)
+            avg_reward += reward
+            state = next_state
+    avg_reward /= eval_episodes
 
-	avg_reward = 0.
-	for _ in range(eval_episodes):
-		state, done = eval_env.reset(), False
-		while not done:
-			action = policy.select_action(np.array(state))
-			state, reward, done, _ = eval_env.step(action)
-			avg_reward += reward
+    print("---------------------------------------")
+    print("[{}] Evaluation over {} episodes: {}".format(eval_cnt, eval_episodes, avg_reward))
+    print("---------------------------------------")
 
-	avg_reward /= eval_episodes
-
-	print("---------------------------------------")
-	print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
-	print("---------------------------------------")
-	return avg_reward
+    return avg_reward  #算法的评估
 
 
 if __name__ == "__main__":
@@ -45,7 +44,7 @@ if __name__ == "__main__":
     parser.add_argument("--steps", default=1e6, type=int, help='Maximum number of steps')
 
     parser.add_argument("--discount", default=0.99, help='Discount factor')
-    parser.add_argument("--la", default=0.8, type=float)
+    parser.add_argument("--lambda", default=0.8, type=float)
     parser.add_argument("--tau", default=0.005, type=float, help='Target network update rate')
 
     parser.add_argument("--alpha", default=0.005, type=float)
@@ -64,7 +63,6 @@ if __name__ == "__main__":
 
     parser.add_argument("--policy-freq", default=2, type=int, help='Frequency of delayed policy updates')
     parser.add_argument("--N-step", default=2, type=int)
-    parser.add_argument("--n-step", default=0, type=int)
 
     args = parser.parse_args()
 
@@ -108,78 +106,86 @@ if __name__ == "__main__":
         "device": device,
         "alpha": args.alpha,
     }
-    if args.policy == "DDPG":
-        policy = DDPG.DDPG(**kwargs)
-
-	elif args.policy == "TD3":
-		# Target policy smoothing is scaled wrt the action scale
-		kwargs["policy_noise"] = args.policy_noise * max_action
-		kwargs["noise_clip"] = args.noise_clip * max_action
-		kwargs["policy_freq"] = args.policy_freq
-		policy = TD3.TD3(**kwargs)
-
-    elif args.policy == "DDPG_N":
-        policy = DDPG_N.DDPG_N(**kwargs)
-
-    elif args.policy == "TD3_N":
+	
+    if args.policy == "TD3_N":
         kwargs["policy_noise"] = args.policy_noise * max_action
         kwargs["noise_clip"] = args.noise_clip * max_action
         kwargs["policy_freq"] = args.policy_freq
 
         policy = TD3_N.TD3_N(**kwargs)
 
-	if args.load_model != "":
-		policy_file = file_name if args.load_model == "default" else args.load_model
-		policy.load(f"./models/{policy_file}")
+    if args.load_model != "":
+        policy.load("./models/{}".format(args.load_model))
 
-	replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
-	
-	# Evaluate untrained policy
-	evaluations = [eval_policy(policy, args.env, args.seed)]
+    #Initialize replay buffer with state and action dimension & buffer size
+    replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
 
-	state, done = env.reset(), False
-	episode_reward = 0
-	episode_timesteps = 0
-	episode_num = 0
+    # Initialize temporary buffer D'
+    exp_buffer = deque()
 
-	for t in range(int(args.max_timesteps)):
-		
-		episode_timesteps += 1
+    eval_cnt = 0
+    eval_return = [eval_policy(policy, args.env, args.seed, eval_cnt=eval_cnt)]
+    eval_cnt += 1
 
-		# Select action randomly or according to policy
-		if t < args.start_timesteps:
-			action = env.action_space.sample()
-		else:
-			action = (
-				policy.select_action(np.array(state))
-				+ np.random.normal(0, max_action * args.expl_noise, size=action_dim)
-			).clip(-max_action, max_action)
+    # Initialize environment
+    state, done = env.reset(), False
 
-		# Perform action
-		next_state, reward, done, _ = env.step(action) 
-		done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
+    # Initialize episode timesteps
+    episode_timesteps = 0
+    episode_reward = 0
+    episode_num = 0
 
-		# Store data in replay buffer
-		replay_buffer.add(state, action, next_state, reward, done_bool)
+    # Start training steps until max timesteps
+    for t in range(int(args.steps)):
+        episode_timesteps += 1
 
-		state = next_state
-		episode_reward += reward
+        # select action randomly or according to policy
+        if t < args.start_steps:
+            action = env.action_space.sample()
+        else:
+            action = (
+                    policy.select_action(np.array(state))
+                    + np.random.normal(0, max_action * args.expl_noise, size=action_dim)
+            ).clip(-max_action, max_action)
 
-		# Train agent after collecting sufficient data
-		if t >= args.start_timesteps:
-			policy.train(replay_buffer, args.batch_size)
+        next_state, reward, done, _ = env.step(action)
+        done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
 
-		if done: 
-			# +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
-			print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
-			# Reset environment
-			state, done = env.reset(), False
-			episode_reward = 0
-			episode_timesteps = 0
-			episode_num += 1 
+        exp_buffer.append((state, action, next_state, reward, done_bool))
 
-		# Evaluate episode
-		if (t + 1) % args.eval_freq == 0:
-			evaluations.append(eval_policy(policy, args.env, args.seed))
-			np.save(f"./results/{file_name}", evaluations)
-			if args.save_model: policy.save(f"./models/{file_name}")
+        state = next_state
+        episode_reward += reward
+
+        if t >= args.start_steps:
+            policy.train(replay_buffer, args.batch_size)
+
+        if done:
+            len_buffer = len(exp_buffer)
+            d_reward = 0
+            for i in range(len_buffer):
+                discounted_reward = 0
+                gamma = args.lambda
+
+                state, action, next_state, _, done_bool = exp_buffer[i]
+                for j in range(i, min(i + args.N_step, len_buffer)):
+                    _, _, _, r, _ = exp_buffer[j]
+                    discounted_reward += r * gamma
+                    gamma *= args.la
+                ds_factor = (1 - args.lambda) / (args.la * (1 - gamma / args.lambda))
+                discounted_reward = ds_factor * discounted_reward
+                d_reward += discounted_reward
+                replay_buffer.add(state, action, next_state, discounted_reward, done_bool)
+            exp_buffer.clear()
+            print(
+                    "Total T: {} Episode Num: {} Episode T: {} discounted_Reward: {} Reward: {}".format(t + 1,
+                                                   episode_num + 1, len_buffer, d_reward, episode_reward))
+
+            state, done = env.reset(), False
+            episode_reward = 0
+            episode_timesteps = 0
+            episode_num += 1
+
+        if (t+1) % args.eval_freq == 0:
+            eval_return.append(eval_policy(policy, args.env, args.seed,  eval_cnt=eval_cnt))
+            np.save(f"./results/{file_name}", eval_return)
+            eval_cnt += 1
